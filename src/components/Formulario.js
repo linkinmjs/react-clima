ac561b1f7759fd3ed3b9a7abd4c1136e9f88d9a4
Mauri <mauricio.stampella@25watts.com.ar>
2020-06-03 20:28:30 -0300

    Creando el State y leyendo lo que el usuario selecciona

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,6 +1,26 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const Formulario = () => {
+
+    //state del formulario
+    const [busqueda, guardarBusqueda] = useState({
+        ciudad: '',
+        pais:''
+    })
+
+    // extraer ciudad y país
+    const {ciudad, pais} = busqueda
+
+    // function que coloca los elementos en el state
+    const handleChange = e => {
+        // actualizar el state
+        guardarBusqueda({
+            ...busqueda,
+            [e.target.name]: e.target.value
+        })
+    }
+
+
     return (
       <form>
           <div className="input-field col s12">
@@ -8,6 +28,8 @@ const Formulario = () => {
                 type="text"
                 name="ciudad"
                 id="ciudad"
+                value={ciudad}
+                onChange={handleChange}
               />
               <label htmlFor="ciudad">Ciudad: </label>
           </div>
@@ -15,9 +37,18 @@ const Formulario = () => {
           <div className="input-field col s12">
               <select
                 name="pais"
+                id="pais"
+                value={pais}
+                onChange={handleChange}
               >
-                  <option value="">-- Seleccione un país --</option>
-
+                <option value="">-- Seleccione un país --</option>
+                <option value="US">Estados Unidos</option>
+                <option value="MX">México</option>
+                <option value="AR">Argentina</option>
+                <option value="CO">Colombia</option>
+                <option value="CR">Costa Rica</option>
+                <option value="ES">España</option>
+                <option value="PE">Perú</option>
               </select>
           </div>
       </form>      
