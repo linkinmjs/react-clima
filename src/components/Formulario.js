96de6abfd364371bf02b383e5af39dde9831b98b
Mauri <mauricio.stampella@25watts.com.ar>
2020-06-03 21:10:28 -0300

    Validando el Formulario

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -7,6 +7,7 @@ const Formulario = () => {
         ciudad: '',
         pais:''
     })
+    const [error, guardarError] = useState(false);
 
     // extraer ciudad y país
     const {ciudad, pais} = busqueda
@@ -20,9 +21,27 @@ const Formulario = () => {
         })
     }
 
+    // cuando el usuario da submit al form
+    const handleSubmit = e => {
+        e.preventDefault();
+
+        // validar
+        if(ciudad.trim() === '' || pais.trim() === ''){
+            guardarError(true);
+            return
+        }
+
+        guardarError(false);
+        
+
+        // pasarlo al componente principal
+    }
 
     return (
-      <form>
+      <form
+        onSubmit={handleSubmit}
+      >
+          {error?<p className="red darken-4 error">Todos los campos son obligatorios</p>:null}
           <div className="input-field col s12">
               <input
                 type="text"
@@ -51,6 +70,14 @@ const Formulario = () => {
                 <option value="PE">Perú</option>
               </select>
           </div>
+        <div className="input-field col s12">
+            <input 
+                type="submit"
+                value="Buscar clima"
+                className="waves-effect waves-light btn-large btn-block yellow accent-4"
+            />
+        </div>
+        
       </form>      
     );
 }
